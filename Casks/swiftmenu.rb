690a36f350626cce33ff2d648df41e081e1e1dc2
paul <423357+toothbrush@users.noreply.github.com>
2022-10-10 22:24:35 -0400

    swiftmenu: 0.3.0

--- Casks/swiftmenu.rb
+++ Casks/swiftmenu.rb
@@ -1,6 +1,6 @@
 cask "swiftmenu" do
-  version "0.2.0"
-  sha256 "6b9472b5fbfb53c8c76fc23f3246f2560039ee11d2e9c42dcb62f44e9f1184f0"
+  version "0.3.0"
+  sha256 "59dfc98710ef6e6ac83641f8e46d07e6a096c3cb0aeacfced66e9a2b97932f8d"
 
   url "https://github.com/toothbrush/SwiftMenu/releases/download/v#{version}/SwiftMenu-v#{version}.zip"
   name "SwiftMenu"
