407ad09c668fc44392da6409a55ca451edc3dc19
paul <423357+toothbrush@users.noreply.github.com>
2023-02-12 15:14:15 +1100

    swiftmenu: Update to 0.5

--- Casks/swiftmenu.rb
+++ Casks/swiftmenu.rb
@@ -1,10 +1,10 @@
 cask "swiftmenu" do
-  version "0.4"
-  sha256 "288e7a888c4ee6c55e1f38eb9f4d1c517a6ea7d33533669c5ca14ee0322b8dc9"
+  version "0.5"
+  sha256 "6afb578a8719f302c9b7eb994db84730cca5ea9fc16844870aea67f323e06f1c"
 
   url "https://github.com/toothbrush/SwiftMenu/releases/download/v#{version}/SwiftMenu-v#{version}.zip"
   name "SwiftMenu"
-  desc "Crappy REST-aware GUI options picker"
+  desc "Crappy GUI options picker"
   homepage "https://github.com/toothbrush/SwiftMenu"
 
   depends_on macos: ">= :catalina"
